16bc2d80cae4973251729a4f34adbdb9a8b02798
Mike Wooldridge <mike@hellion.org>
2014-10-02 23:14:35 -0700

    start of validation on submit

--- browser/src/app/states/ask.js
+++ browser/src/app/states/ask.js
@@ -10,6 +10,9 @@ define(['app/module'], function (module) {
       ssQnaDoc.create().attachScope($scope, 'qnaDoc');
 
       $scope.save = function () {
+        // How does the following method work? Pass itself as arg?
+        // Shouldn't it know to test its current state?
+        window.console.log($scope.qnaDoc.validateObject());
         $scope.qnaDoc.post().$ml.waiting.then(function () {
           appRouting.go('^.qnaDoc', {id: $scope.qnaDoc.id});
         });
