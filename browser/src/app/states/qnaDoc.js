31dd9b712a679cd8ae3d2dfa81cc4a34a7fc89cf
Mike Wooldridge <mike@hellion.org>
2014-10-16 17:34:04 -0700

    update qnaDoc controller to handle answer submission

--- browser/src/app/states/qnaDoc.js
+++ browser/src/app/states/qnaDoc.js
@@ -6,11 +6,13 @@ define(['app/module'], function (module) {
     'marked',
     'appRouting',
     'ssQnaDoc',
+    'ssAnswer',
     function (
       $scope,
       marked,
       appRouting,
-      ssQnaDoc
+      ssQnaDoc,
+      ssAnswer
     ) {
 
       $scope.setLoading(true);
@@ -48,6 +50,7 @@ define(['app/module'], function (module) {
             }
           }
         );
+        ssAnswer.create({}, doc).attachScope($scope, 'newAnswer');
       };
 
       $scope.answersCountLabel = function () {
@@ -64,6 +67,25 @@ define(['app/module'], function (module) {
 
       $scope.setQueryText = function (text) {
         appRouting.go('^.explore', { q: $scope.searchbarText });
+      };
+
+      $scope.saveAnswer = function () {
+        if ($scope.newAnswer.$ml.valid) {
+          $scope.newAnswer.post().$ml.waiting.then(function () {
+            $scope.newAnswer.text = ''; // Clear answer form field
+            appRouting.go('^.qnaDoc', {id: $scope.doc.id});
+          },
+          function (error) {
+            if (error.status === 401) {
+              $scope.setLocalError(
+                'User does not have permission to answer questions'
+              );
+            }
+            else {
+              throw new Error('Error occurred: ' + JSON.stringify(error));
+            }
+          });
+        }
       };
 
       $scope.setPageTitle('doc');
