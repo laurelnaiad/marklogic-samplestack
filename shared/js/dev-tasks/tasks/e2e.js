ffe6e107b7a010aa051204d2060c21f7fe8903db
Daphne Maddox <laurelnaiad@gmail.com>
2015-04-17 09:16:16 -0700

    e2e: proactively close child processes when e2e process ends
    On process exit for e2e task, call ctx.closeActiveServers so that child
    processes used during e2e testing (which may include selenium, an
    express server and/or a java server) are explicitly closed as the parent
    process exits.
    
    Closes #669

--- shared/js/dev-tasks/tasks/e2e.js
+++ shared/js/dev-tasks/tasks/e2e.js
@@ -1,18 +1,18 @@
-/* 
- * Copyright 2012-2015 MarkLogic Corporation 
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); 
- * you may not use this file except in compliance with the License. 
- * You may obtain a copy of the License at 
- * 
- *    http://www.apache.org/licenses/LICENSE-2.0 
- * 
- * Unless required by applicable law or agreed to in writing, software 
- * distributed under the License is distributed on an "AS IS" BASIS, 
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
- * See the License for the specific language governing permissions and 
- * limitations under the License. 
- */ 
+/*
+ * Copyright 2012-2015 MarkLogic Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 var path = require('path');
 var _ = require('lodash');
@@ -112,6 +112,9 @@ myTasks.push({
   deps: ['build', 'selenium-start', 'middle-tier-start'],
   func: function (cb) {
     try {
+
+      process.on('exit', ctx.closeActiveServers);
+
       ctx.startServer(
         ctx.paths.browser.buildDir,
         ctx.options.envs.e2e.addresses.webApp.port
