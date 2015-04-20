c8c8575d27ee33dc1d93d87c29f1c339df45cc79
Daphne Maddox <laurelnaiad@gmail.com>
2015-04-20 01:03:53 -0700

    asdf

--- shared/js/dev-tasks/context.js
+++ shared/js/dev-tasks/context.js
@@ -296,6 +296,7 @@ self = module.exports = {
         }),
         // when all have called back, call back the caller
         function () {
+          console.log('calling back');
           activeServers = null;
           closing = false;
           callback();
